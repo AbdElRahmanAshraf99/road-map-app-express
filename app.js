89c84a5545cec6e0b16e4247bc6009cc114528a3
AbdElRahmanAshraf99 <abdoalaraby74@outlook.com>
2023-09-07 15:07:44 +0300

    Testing JSON Get Request First Time

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-import {fetchJavaFundamentalSubTopic} from "./javaFundamentals";
 
 const express = require("express");
 const app = express();
@@ -11,3 +10,21 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
+function fetchJavaFundamentalSubTopic(subTopic) {
+    if (subTopic === "Functions")
+        return {
+            mdFileData: 'A **Function (Method)** is a way to perform some task. Similarly, in programming like Java, a function method is a block of code written to perform a\n' +
+                'specific task repeatedly. It provides reusability of code. We write the function once and use it many times. It works on the ‘DRY’ principle i.e., “Do\n' +
+                'not repeat yourself”.\n' +
+                '\n' +
+                '#### What Should you do?\n' +
+                '\n' +
+                '- Method Declaration\n' +
+                '- Method Call\n' +
+                '\n' +
+                '#### Method Declaration\n' +
+                '\n' +
+                'The method declaration provides information about method attributes, such as visibility, return-type, name, and arguments. It has six components that\n' +
+                'are known as method header, as we have shown in the following figure.\n'
+        }
+}
